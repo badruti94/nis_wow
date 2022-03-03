d25435e3074b5eab0e3831cde1a3ca95ed50cc3d
Badru <badruakfm@gmail.com>
2022-03-03 16:02:31 +0700

    add login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Input } from 'reactstrap'
 
-const Login = () => {
+const Login = ({ isOpen, setIsOpen }) => {
     return (
-        <div>Login</div>
+        <div>
+            <Modal isOpen={isOpen}
+                toggle={function noRefCheck() { }}
+            >
+                <ModalHeader toggle={() => { setIsOpen(false) }}>
+                    <h2 className='font-weight-bold mb-lg-3' >Sign In</h2>
+                </ModalHeader>
+                <ModalBody>
+                    <Form>
+                        <FormGroup>
+                            <Input placeholder='Email' />
+                        </FormGroup>
+                        <FormGroup>
+                            <Input placeholder='Password' />
+                        </FormGroup>
+                        <Button className='mt-lg-3 mb-lg-3' color='danger' >Sign In</Button>
+                        <p>Don't have a account? Klik <a href="#">here</a></p>
+                    </Form>
+                </ModalBody>
+            </Modal>
+        </div>
     )
 }
 
